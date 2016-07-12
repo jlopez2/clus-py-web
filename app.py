5a48982f3a605b63a84e55539e0f76803ee5c529
James Lopez <jimmylop2003@yahoo.com>
2016-07-12 16:42:58 -0700

    update flask

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!!"
+#@app.route("/")
+#def hello():
+#    return "Hello World!!"
 
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+#if __name__ == "__main__":
+#    app.run(debug=True,host='0.0.0.0')
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': ‘James’}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
+
