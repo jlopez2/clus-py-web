fadf238221374959d654d2f1e996bb12d1f08c9e
James Lopez <jimmylop2003@yahoo.com>
2016-07-14 11:57:51 -0700

    revert

--- app.py
+++ app.py
@@ -2,21 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello Cisco World !!"
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': ‘James’}  # fake user
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
